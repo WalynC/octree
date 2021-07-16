417b1e4fa89cae54ec95ab2f9c3e82856c576622
Walyn <walyncurtis@gmail.com>
2021-07-16 16:11:03 -0500

    Removes unused code

--- octree/Assets/FirstPersonCamera.cs
+++ octree/Assets/FirstPersonCamera.cs
@@ -34,7 +34,6 @@ public class FirstPersonCamera : MonoBehaviour
             float maxSDisp = Screen.width / 2f;
             Vector2 screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
             if (Screen.width > Screen.height) { maxSDisp = Screen.height / 2; }
-            float angle = Mathf.Atan2(screenCenter.y - Input.mousePosition.y, screenCenter.x - Input.mousePosition.x);
             Vector2 pos = new Vector2(screenCenter.y - Input.mousePosition.y, screenCenter.x - Input.mousePosition.x).normalized;
             float disp = Vector2.Distance(screenCenter, new Vector2(Input.mousePosition.x, Input.mousePosition.y));
             float scale = Mathf.Clamp(disp / maxSDisp, 0, 1);
